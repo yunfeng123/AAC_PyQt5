471ef265222dbdb7a760e2377f12a82fbe43678d
yunfeng123 <guan_yunfeng@sina.com>
2021-07-20 22:27:26 +0800

    set fixed size of mainwindow

--- main_window_rc.py
+++ main_window_rc.py
@@ -14,7 +14,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
+        #MainWindow.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint)
         MainWindow.resize(680, 408)
+        MainWindow.setFixedSize(MainWindow.width(), MainWindow.height())
         font = QtGui.QFont()
         font.setFamily("Arial")
         MainWindow.setFont(font)
